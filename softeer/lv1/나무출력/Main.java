17806b1a298f0e0227bb26fcb3eeb1dcf9176354
박준규 <junju404@naver.com>
2024-11-01 00:42:40 +0900

    문자열 개행 사용한 출력방법

---
+++ softeer/lv1/나무출력/Main.java
@@ -0,0 +1,26 @@
+package softeer.lv1.나무출력;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String answer = 
+            "           *********           \n"+
+            "      *****         ****       \n"+
+            "    **                  **     \n"+
+            "   *                      *    \n"+
+            " **                        **  \n"+
+            "*                            * \n"+
+            "*                            * \n"+
+            "**                          ** \n"+
+            " **  *    ****  ****    *  **  \n"+
+            "   ** ***** ****** ***** **    \n"+
+            "             ****              \n"+
+            "             ****              \n"+
+            "             ****              \n"+
+            "             ****              \n"+
+            "            ******             \n"+
+            "           ********";
+
+        System.out.print(answer);
+    }
+}
